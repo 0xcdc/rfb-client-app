e9ff03571bad82a7a50253fbfdb69e92fd6caa02
Charlie Carson <charliecarson@gmail.com>
2016-08-06 13:01:02 -0700

    changed web page titles

--- src/routes/clientDetail/ClientDetail.js
+++ src/routes/clientDetail/ClientDetail.js
@@ -12,7 +12,7 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './ClientDetail.css';
 import ClientDetailForm from '../../components/ClientDetailForm';
 
-const title = 'Client Details';
+const title = 'Renewel Food Bank'
 
 function ClientDetail(props, context) {
   context.setTitle(title);
