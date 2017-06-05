879864fe6e5c1571fa5134b95e1fd684391aaeba
Charlie Carson <charliecarson@gmail.com>
2017-06-04 17:35:44 -0700

    added buttons for home and reports

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,13 +11,19 @@ import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import Link from '../Link';
 import s from './Header.css';
+import { Button, Glyphicon } from 'react-bootstrap';
 
 class Header extends React.Component {
   render() {
     return (
+      <div>
+        <Link to="/"><Button>Home <Glyphicon glyph='home'/></Button></Link>
+        <Link to="/report"><Button>Reports <Glyphicon glyph='book'/></Button></Link>
+        <br/>
         <Link to='/'>
            <img src="/default-logo.png" className="img-responsive"/>
         </Link>
+      </div>
     );
   }
 }
