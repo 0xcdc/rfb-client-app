e89c653bcb3ec6e3136c0c16b149838e7c09d1f1
Charlie Carson <charliecarson@gmail.com>
2019-01-19 21:53:00 -0800

    disable string operators in sequelize

--- src/data/root.js
+++ src/data/root.js
@@ -10,10 +10,13 @@
 import Sequelize from 'sequelize';
 import { databaseUrl } from '../config';
 
-const sequelize = new Sequelize(databaseUrl, {
-  define: {
-    freezeTableName: true,
-  },
-});
+const sequelize = new Sequelize(
+  databaseUrl, {
+    define: {
+      freezeTableName: true,
+    },
+    operatorsAliases: false,
+  }
+);
 
 export default sequelize;
